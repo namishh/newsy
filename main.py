8e1dfea39bbaf0944e9d2173dbd1107efa48db73
Trey Thomas <TreyThomas93@gmail.com>
2021-03-20 18:54:42 -0500

    commit

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+"""
+REMOVED UNNECCESARY EXCEPTION CODE
+REMOVED LAMBDA FROM BUTTON COMMANDS
+INSTALLED DOTENV TO ALLOW IMPORTATION OF SECURE INFORMATION I.E API_KEY
+YOU SHOULD NEVER SET ANY SECRET KEYS, API KEYS, ACCESS TOKENS, ECT INTO YOUR CODE.
+I HAVE ADDED A .ENV FILE AND SET IT IN .GITIGNORE SO YOU WONT BE ABLE TO COMMIT THE FILE TO LOCAL NOT REMOTE. 
+YOU WILL NEED TO MAKE A HABIT OF THIS ON ALL PROJECTS.
+"""
+
+
 from newsapi.newsapi_client import NewsApiClient
 import tkinter as tk
 import webbrowser
+from dotenv import load_dotenv # pip install python-dotenv
+import os
+
+API_KEY = os.getenv('API_KEY')
 
 root = tk.Tk()
 root.geometry("1920x1080")
 root.title("News App")
 
-newsapi = NewsApiClient(api_key='9319a21a7472491897d5efd548f290dc')
+newsapi = NewsApiClient(api_key=API_KEY)
 
 label = tk.Label(root, text="", font=('Helvetica', 15))
 label.place(relx=0, rely=0.25, relheight='0.7', relwidth='1')
@@ -33,7 +47,9 @@ def get_url():
         webpage2 = clicked2.get()
         webbrowser.open(webpage2)
         webbrowser.open(webpage)
-    except Exception as e:
+
+    except:
+
         pass
 
 
@@ -49,8 +65,9 @@ def get_headlines():
             li.append(news['title'])
 
         # li2 = [news['title'] + " " + news['url'] for news in top_headlines['articles']]
-        label.config(text=(" ".join([news['title'] + "\n" for news in top_headlines['articles']])))
-    except Exception as e:
+        label.config(
+            text=(" ".join([news['title'] + "\n" for news in top_headlines['articles']])))
+    except:
         label.config(text=("There was error in processing your request."))
 
 
@@ -65,8 +82,9 @@ def get_news():
             # print(news['title'] +  " Url = " + news['url'])
             li.append(news['title'])
 
-        label.config(text=" ".join([news['title'] + "\n" for news in all_articles['articles']]))
-    except Exception as e:
+        label.config(text=" ".join(
+            [news['title'] + "\n" for news in all_articles['articles']]))
+    except:
         label.config(text=("There was error in processing your request."))
 
 
@@ -81,8 +99,9 @@ def search_query():
 
         # li2 = [news['title'] + " " + news['url'] for news in top_headlines['articles']]
 
-        label.config(text=(" ".join([news['title'] + "\n" for news in top_headlines1['articles']])))
-    except Exception as e:
+        label.config(
+            text=(" ".join([news['title'] + "\n" for news in top_headlines1['articles']])))
+    except:
         label.config(text=("There was error in processing your request."))
 
 
@@ -92,19 +111,23 @@ drop.place(relx=0, rely=0)
 drop2 = tk.OptionMenu(root, clicked2, *li2)
 drop2.place(relx=0, rely=0.1)
 
-url_button = tk.Button(root, text="Select", bd=1, activebackground="#ffee96", command=lambda: get_url())
+url_button = tk.Button(root, text="Select", bd=1,
+                       activebackground="#ffee96", command=get_url)
 url_button.place(relx=0.8, rely=0)
 
-headline = tk.Button(root, text="India News", bd=1, activebackground="#ffee96", command=lambda: get_headlines())
+headline = tk.Button(root, text="India News", bd=1,
+                     activebackground="#ffee96", command=get_headlines)
 headline.place(relx=0.3, rely=0.2)
 
-all_news = tk.Button(root, text="World News", bd=1, activebackground="#ffee96", command=lambda: get_news())
+all_news = tk.Button(root, text="World News", bd=1,
+                     activebackground="#ffee96", command=get_news)
 all_news.place(relx=0.4, rely=0.2)
 
 query = tk.Entry(root, font=('Courier', 10), bd=0)
 query.place(relx=0.5, rely=0.2)
 
-search = tk.Button(root, text="Search", bd=1, activebackground="#ffee96", command=lambda: search_query())
+search = tk.Button(root, text="Search", bd=1,
+                   activebackground="#ffee96", command=search_query)
 search.place(relx=0.65, rely=0.2)
 
 root.mainloop()
